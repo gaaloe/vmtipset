328ee6ee2bb2a1d13846c5eec53fbbc2f8d58759
gaaloe <henrik.johansson@allt3.se>
2018-06-20 19:45:47 +0200

    Skiljer på SIZE_ROW och NR_COMBS

--- manager_code.cpp
+++ manager_code.cpp
@@ -1,38 +1,39 @@
 #include <mpi.h>
 #include <iostream>
+#include <cassert>
 #define MIN( x, y ) ((x) < (y) ? x : y)
 enum e_team {rus, alg, eng, irn};
 enum e_person {sthrjo, stanny};
-#define SIZE 3
-e_team game_result[SIZE] = {rus, alg, eng};
+#define SIZE_ROW 3
+// Must be 2^SIZE_ROW
+#define NR_COMBS 8
+e_team game_result[SIZE_ROW] = {rus, alg, eng};
 
 void manager_code( int numprocs )
 {
-    double a[SIZE][SIZE], c[SIZE];
-
     int i, sender, row, numsent = 0;
     e_person dotp;
     MPI_Status status;
 
-    for ( i = 1; i < MIN( numprocs, SIZE ); i++ ) {
-        MPI_Send( game_result, SIZE, MPI_INT, i, i, MPI_COMM_WORLD );
+    for ( i = 1; i < MIN( numprocs, NR_COMBS ); i++ ) {
+        MPI_Send( game_result, SIZE_ROW, MPI_INT, i, i, MPI_COMM_WORLD );
         numsent++;
     }
-    if (SIZE < numprocs) {
-      for ( i = SIZE; i < numprocs; i++ ) {
+    if (NR_COMBS < numprocs) {
+      for ( i = NR_COMBS; i < numprocs; i++ ) {
             MPI_Send( MPI_BOTTOM, 0, MPI_INT, i, 0,
                        MPI_COMM_WORLD );
       }
     }
     /* receive X back from workers */
-    for ( i = 0; i < SIZE; i++ ) {
+    for ( i = 0; i < NR_COMBS; i++ ) {
         MPI_Recv( &dotp, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG,
                   MPI_COMM_WORLD, &status );
         sender = status.MPI_SOURCE;
         row    = status.MPI_TAG - 1;
-        /* send another Y back to this worker if there is one */
-        if ( numsent < SIZE ) {
-            MPI_Send( game_result, SIZE, MPI_INT, sender,
+        /* send another piece of work to this worker if there is one */
+        if ( numsent < NR_COMBS ) {
+            MPI_Send( game_result, SIZE_ROW, MPI_INT, sender,
                       numsent + 1, MPI_COMM_WORLD );
             numsent++;
         }
@@ -40,4 +41,5 @@ void manager_code( int numprocs )
             MPI_Send( MPI_BOTTOM, 0, MPI_INT, sender, 0,
                        MPI_COMM_WORLD );
     }
+    assert(numsent == NR_COMBS);
 }
--- worker_code.cpp
+++ worker_code.cpp
@@ -1,31 +1,36 @@
 #include <mpi.h>
 #include <iostream>
 #include <cassert>
-#define SIZE 3
+#define SIZE_ROW 3
+// Must be 2^SIZE_ROW
+#define NR_COMBS 8
 enum e_team {rus, alg, eng, irn};
 enum e_person {sthrjo, stanny};
 void worker_code( void )
 {
-    e_team b[SIZE], c[SIZE];
+    e_team c[SIZE_ROW];
     int i, row, myrank;
     e_person dotp;
     MPI_Status status;
 
     MPI_Comm_rank( MPI_COMM_WORLD, &myrank );
-    if ( myrank <= SIZE ) {
-        MPI_Recv( c, SIZE, MPI_INT, 0, MPI_ANY_TAG,
+    if ( myrank <= NR_COMBS ) {
+        MPI_Recv( c, SIZE_ROW, MPI_INT, 0, MPI_ANY_TAG,
                   MPI_COMM_WORLD, &status );
         while ( status.MPI_TAG > 0 ) {
             row = status.MPI_TAG - 1;
             dotp = sthrjo;
             MPI_Send( &dotp, 1, MPI_INT, 0, row + 1,
                       MPI_COMM_WORLD );
-            MPI_Recv( c, SIZE, MPI_INT, 0, MPI_ANY_TAG,
+            MPI_Recv( c, SIZE_ROW, MPI_INT, 0, MPI_ANY_TAG,
                       MPI_COMM_WORLD, &status );
         }
         assert(status.MPI_TAG == 0); // Proper end-of-execution
     } else {
         // There are more processors than actually needed
+        MPI_Recv( c, SIZE_ROW, MPI_INT, 0, MPI_ANY_TAG,
+                  MPI_COMM_WORLD, &status );
+        assert(status.MPI_TAG == 0); // Proper end-of-execution
     }
 }
 
