8a1682d330ffc76438fc411edd2bbb007d864653
gaaloe <henrik.johansson@allt3.se>
2018-06-20 18:39:14 +0200

    Fil från http://etutorials.org/Linux+systems/cluster+computing+with+linux/Part+II+Parallel+Programming/Chapter+8+Parallel+Programming+with+MPI/8.2+Manager+Worker+Example/

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include "mpi.h"
+
+int main( int argc, char *argv[] )
+{
+    int numprocs, myrank;
+
+    MPI_Init( &argc, &argv );
+    MPI_Comm_size( MPI_COMM_WORLD, &numprocs );
+    MPI_Comm_rank( MPI_COMM_WORLD, &myrank );
+
+    if ( myrank == 0 )          /* manager process */
+        manager_code ( numprocs );
+    else                        /* worker process */
+        worker_code ( );
+    MPI_Finalize( );
+    return 0;
+}
