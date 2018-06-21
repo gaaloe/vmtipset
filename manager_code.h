ec85e498248a15f5a20c933a1518e5ce59821bb8
gaaloe <henrik.johansson@allt3.se>
2018-06-21 17:24:42 +0200

    Brytit ut en header, indenterat

---
+++ manager_code.h
@@ -0,0 +1,10 @@
+#ifndef manager_code_h
+#define manager_code_h
+enum e_team {rus, ksa, egy, uru,
+             mar, irn, por, esp,
+             fra, aus, per, den,
+             arg, isl, cro, nga};
+enum e_person {sthrjo, stanny};
+#define SIZE_ROW 15
+#define NR_COMBS (4*3*4*3*4*3*4*3*2*2*2*2*2*2*2)
+#endif
