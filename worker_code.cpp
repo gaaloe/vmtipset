0a93f055716e1176aaae0a7714f2f92c8799ca22
gaaloe <henrik.johansson@allt3.se>
2018-06-23 17:22:48 +0200

    Kontrollräknat en rad för sthrjo, gav 6X7+2X10 korrektZ

--- worker_code.cpp
+++ worker_code.cpp
@@ -22,16 +22,25 @@ void worker_code( void )
     MPI_Recv( c, SIZE_ROW, MPI_INT, 0, MPI_ANY_TAG,
 	      MPI_COMM_WORLD, &status );
     while ( status.MPI_TAG > 0 ) {
-      int score = 0;
-      for (e_team i = (e_team)0; i < (e_team)16; ++i) {
+      int score_16 = 0;
+      for (e_team i = (e_team)0; i < (e_team)16; ++i, ++i) {
+         if (c[i] == HRJO[i]) {
+           score_16 += 10;
+         } else if (c[i] == HRJO[i+1]) {
+           score_16 += 7;
+         }
+         if (c[i+1] == HRJO[i+1]) {
+           score_16 += 10;
+         } else if (c[i+1] == HRJO[i]) {
+           score_16 += 7;
+         }
+#if 0
+         std::cout << c[i]<< ' ' << HRJO[i] << ' ';
+         std::cout << c[i+1]<< ' ' << HRJO[i+1] << ',';
+#endif
       }
 #if 0
-      std::cout << c[0]<< ' ' << c[1] << ' ' << c[2] << ' ';
-      std::cout << c[3]<< ' ' << c[4] << ' ' << c[5] << ' ';
-      std::cout << c[6]<< ' ' << c[7] << ' ' << c[8] << ' ';
-      std::cout << c[9]<< ' ' << c[10] << ' ' << c[11] << ' ';
-      std::cout << c[12]<< ' ' << c[13] << ' ' << c[14] << ' ';
-      std::cout << std::endl;
+      std::cout << score_16 <<std::endl;
 #endif
       int tag = status.MPI_TAG;
       dotp[0] = sthrjo;
