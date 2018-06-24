17f2aa1945053624beb76ee37ae87e7c92c928e8
gaaloe <henrik.johansson@allt3.se>
2018-06-24 20:22:20 +0200

    Bytt primtal

--- manager_code.h
+++ manager_code.h
@@ -61,7 +61,7 @@ enum e_person {
 #define NR_COMBS 48922361856L
 //It is probably good if JUMP_HASH is a prime number (?). See..
 //https://primes.utm.edu/lists/small/10000.txt
-#define JUMP_HASH        25111L
+#define JUMP_HASH        12503L
 extern e_team operator++(e_team& that);
 extern e_team operator++(e_team& that, int);
 extern std::ostream& operator<<(std::ostream& o, enum e_team);
