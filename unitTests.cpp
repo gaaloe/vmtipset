1d96fbce1c5a9381f613ad438b9e51bc8dd6699f
gaaloe <henrik.johansson@allt3.se>
2018-06-25 19:39:12 +0200

    En test med full pott

--- unitTests.cpp
+++ unitTests.cpp
@@ -36,3 +36,32 @@ public:
 };
 REGISTER_FIXTURE( upperLeftIndented );
 
+class exactLeftIndented : public TestFixture<exactLeftIndented>
+{
+  void setUp() {
+  }
+  void tearDown() {
+  }
+public:
+  TEST_FIXTURE( exactLeftIndented )
+  {
+    TEST_CASE( stmibo );
+  }
+  void stmibo()
+  {
+    e_team c[32] =
+      {rus, uru, por, esp, fra, aus, cro, arg, bra, sui, ger, mex, bel, eng, col, pol,
+       rus, por, fra, cro, bra, ger, bel, eng,
+       fra, cro, bra, eng,
+       bra, eng,
+       cro,
+       bra};
+    const int score = personMatch(STMIBO, c, 2);
+    ASSERT(score == 520);
+    e_person dotp[32];
+    const int siz = whoMatchesBest(c, dotp, 2);
+    ASSERT(siz == 1);
+    ASSERT(dotp[0] == STMIBO);
+  }
+};
+REGISTER_FIXTURE( exactLeftIndented );
