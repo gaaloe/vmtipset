62a27454408155f8d3d7bd0134c16d3ef04112a2
gaaloe <henrik.johansson@allt3.se>
2018-06-25 20:01:02 +0200

    Nu tar körningen 1m20s

--- manager_code.h
+++ manager_code.h
@@ -61,7 +61,7 @@ enum e_person {
 #define NR_COMBS 2038431744L
 //It is probably good if JUMP_HASH is a prime number (?). See..
 //https://primes.utm.edu/lists/small/10000.txt
-#define JUMP_HASH        73L
+#define JUMP_HASH        23L
 extern e_team operator++(e_team& that);
 extern e_team operator++(e_team& that, int);
 extern e_person operator++(e_person& that);
