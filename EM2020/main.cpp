955c3bc239344cf8352b9ccee1d71a34898d6e10
gaaloe <henrik.johansson@allt3.se>
2021-06-02 21:44:47 +0200

    Alla grupper med koll av 1,2,3

--- EM2020/main.cpp
+++ EM2020/main.cpp
@@ -1,14 +1,17 @@
+#include <cassert>
 #include <cinttypes>
+#include <iomanip>
 #include <iostream>
-int main()
-{
+// Format using clang-format -i main.cpp
+int main() {
   uint64_t generator;
   const uint64_t upperlimit = 1L << 55;
   const uint64_t ettPrimtal = 16127L;
   uint64_t printout = 0L;
-  for (uint64_t iteration = 0L; iteration < upperlimit; iteration += ettPrimtal) {
+  for (uint64_t iteration = 0L; iteration < upperlimit;
+       iteration += ettPrimtal) {
     // Group A
-    switch(iteration & 0x000000000000003F) {
+    switch (iteration & 0x000000000000003F) {
     case 0x00:
     case 0x01:
     case 0x02:
@@ -77,113 +80,382 @@ int main()
     case 0x39:
       break;
     default:
-      std::cerr << __FILE__<<__LINE__<<'\n';
+      std::cerr << __FILE__ << __LINE__ << '\n';
       abort();
     }
     // Group B
-    switch(iteration & 0x0000000000000FC0) {
-    case 0x00<<6:
-    case 0x01<<6:
-    case 0x02<<6:
-    case 0x03<<6:
-    case 0x04<<6:
-    case 0x05<<6:
-    case 0x08<<6:
-    case 0x0A<<6:
-    case 0x0C<<6:
-    case 0x0F<<6:
-    case 0x10<<6:
-    case 0x11<<6:
-    case 0x14<<6:
-    case 0x15<<6:
-    case 0x16<<6:
-    case 0x17<<6:
-    case 0x19<<6:
-    case 0x1A<<6:
-    case 0x1D<<6:
-    case 0x1F<<6:
-    case 0x20<<6:
-    case 0x22<<6:
-    case 0x25<<6:
-    case 0x26<<6:
-    case 0x28<<6:
-    case 0x29<<6:
-    case 0x2A<<6:
-    case 0x2B<<6:
-    case 0x2E<<6:
-    case 0x2F<<6:
-    case 0x30<<6:
-    case 0x33<<6:
-    case 0x35<<6:
-    case 0x37<<6:
-    case 0x3A<<6:
-    case 0x3B<<6:
-    case 0x3C<<6:
-    case 0x3D<<6:
-    case 0x3E<<6:
-    case 0x3F<<6:
+    switch (iteration & 0x0000000000000FC0) {
+    case 0x00L << 6:
+    case 0x01L << 6:
+    case 0x02L << 6:
+    case 0x03L << 6:
+    case 0x04L << 6:
+    case 0x05L << 6:
+    case 0x08L << 6:
+    case 0x0AL << 6:
+    case 0x0CL << 6:
+    case 0x0FL << 6:
+    case 0x10L << 6:
+    case 0x11L << 6:
+    case 0x14L << 6:
+    case 0x15L << 6:
+    case 0x16L << 6:
+    case 0x17L << 6:
+    case 0x19L << 6:
+    case 0x1AL << 6:
+    case 0x1DL << 6:
+    case 0x1FL << 6:
+    case 0x20L << 6:
+    case 0x22L << 6:
+    case 0x25L << 6:
+    case 0x26L << 6:
+    case 0x28L << 6:
+    case 0x29L << 6:
+    case 0x2AL << 6:
+    case 0x2BL << 6:
+    case 0x2EL << 6:
+    case 0x2FL << 6:
+    case 0x30L << 6:
+    case 0x33L << 6:
+    case 0x35L << 6:
+    case 0x37L << 6:
+    case 0x3AL << 6:
+    case 0x3BL << 6:
+    case 0x3CL << 6:
+    case 0x3DL << 6:
+    case 0x3EL << 6:
+    case 0x3FL << 6:
       continue;
       break;
-    case 0x06<<6:
-    case 0x07<<6:
-    case 0x09<<6:
-    case 0x0B<<6:
-    case 0x0D<<6:
-    case 0x0E<<6:
-    case 0x12<<6:
-    case 0x13<<6:
-    case 0x18<<6:
-    case 0x1B<<6:
-    case 0x1C<<6:
-    case 0x1E<<6:
-    case 0x21<<6:
-    case 0x23<<6:
-    case 0x24<<6:
-    case 0x27<<6:
-    case 0x2C<<6:
-    case 0x2D<<6:
-    case 0x31<<6:
-    case 0x32<<6:
-    case 0x34<<6:
-    case 0x36<<6:
-    case 0x38<<6:
-    case 0x39<<6:
+    case 0x06L << 6:
+    case 0x07L << 6:
+    case 0x09L << 6:
+    case 0x0BL << 6:
+    case 0x0DL << 6:
+    case 0x0EL << 6:
+    case 0x12L << 6:
+    case 0x13L << 6:
+    case 0x18L << 6:
+    case 0x1BL << 6:
+    case 0x1CL << 6:
+    case 0x1EL << 6:
+    case 0x21L << 6:
+    case 0x23L << 6:
+    case 0x24L << 6:
+    case 0x27L << 6:
+    case 0x2CL << 6:
+    case 0x2DL << 6:
+    case 0x31L << 6:
+    case 0x32L << 6:
+    case 0x34L << 6:
+    case 0x36L << 6:
+    case 0x38L << 6:
+    case 0x39L << 6:
       break;
     default:
-      std::cerr << __FILE__<<__LINE__<<'\n';
+      std::cerr << __FILE__ << __LINE__ << '\n';
       abort();
     }
-    if ((iteration & 0x00000000000000C0) == (iteration & 0x0000000000000300)) {
-      continue;
-    }
-    if ((iteration & 0x0000000000000C00) == (iteration & 0x000000000000C000)) {
-      continue;
-    }
-    if ((iteration & 0x0000000000000C00) == (iteration & 0x0000000000003000)) {
-      continue;
-    }
     // Group C
-    if ((iteration & 0x0000000000000300) == (iteration & 0x0000000000000C00)) {
+    switch (iteration & 0x000000000003F000) {
+    case 0x00L << 12:
+    case 0x01L << 12:
+    case 0x02L << 12:
+    case 0x03L << 12:
+    case 0x04L << 12:
+    case 0x05L << 12:
+    case 0x08L << 12:
+    case 0x0AL << 12:
+    case 0x0CL << 12:
+    case 0x0FL << 12:
+    case 0x10L << 12:
+    case 0x11L << 12:
+    case 0x14L << 12:
+    case 0x15L << 12:
+    case 0x16L << 12:
+    case 0x17L << 12:
+    case 0x19L << 12:
+    case 0x1AL << 12:
+    case 0x1DL << 12:
+    case 0x1FL << 12:
+    case 0x20L << 12:
+    case 0x22L << 12:
+    case 0x25L << 12:
+    case 0x26L << 12:
+    case 0x28L << 12:
+    case 0x29L << 12:
+    case 0x2AL << 12:
+    case 0x2BL << 12:
+    case 0x2EL << 12:
+    case 0x2FL << 12:
+    case 0x30L << 12:
+    case 0x33L << 12:
+    case 0x35L << 12:
+    case 0x37L << 12:
+    case 0x3AL << 12:
+    case 0x3BL << 12:
+    case 0x3CL << 12:
+    case 0x3DL << 12:
+    case 0x3EL << 12:
+    case 0x3FL << 12:
       continue;
-    }
-    if ((iteration & 0x0000000000003000) == (iteration & 0x0000000000000300)) {
-      continue;
-    }
-    if ((iteration & 0x0000000000003000) == (iteration & 0x0000000000000C00)) {
-      continue;
+      break;
+    case 0x06L << 12:
+    case 0x07L << 12:
+    case 0x09L << 12:
+    case 0x0BL << 12:
+    case 0x0DL << 12:
+    case 0x0EL << 12:
+    case 0x12L << 12:
+    case 0x13L << 12:
+    case 0x18L << 12:
+    case 0x1BL << 12:
+    case 0x1CL << 12:
+    case 0x1EL << 12:
+    case 0x21L << 12:
+    case 0x23L << 12:
+    case 0x24L << 12:
+    case 0x27L << 12:
+    case 0x2CL << 12:
+    case 0x2DL << 12:
+    case 0x31L << 12:
+    case 0x32L << 12:
+    case 0x34L << 12:
+    case 0x36L << 12:
+    case 0x38L << 12:
+    case 0x39L << 12:
+      break;
+    default:
+      std::cerr << __FILE__ << __LINE__ << '\n';
+      abort();
     }
     // Group D
-    if ((iteration & 0x000000000000C000) == (iteration & 0x0000000000030000)) {
+    switch (iteration & 0x0000000000FC0000) {
+    case 0x00L << 18:
+    case 0x01L << 18:
+    case 0x02L << 18:
+    case 0x03L << 18:
+    case 0x04L << 18:
+    case 0x05L << 18:
+    case 0x08L << 18:
+    case 0x0AL << 18:
+    case 0x0CL << 18:
+    case 0x0FL << 18:
+    case 0x10L << 18:
+    case 0x11L << 18:
+    case 0x14L << 18:
+    case 0x15L << 18:
+    case 0x16L << 18:
+    case 0x17L << 18:
+    case 0x19L << 18:
+    case 0x1AL << 18:
+    case 0x1DL << 18:
+    case 0x1FL << 18:
+    case 0x20L << 18:
+    case 0x22L << 18:
+    case 0x25L << 18:
+    case 0x26L << 18:
+    case 0x28L << 18:
+    case 0x29L << 18:
+    case 0x2AL << 18:
+    case 0x2BL << 18:
+    case 0x2EL << 18:
+    case 0x2FL << 18:
+    case 0x30L << 18:
+    case 0x33L << 18:
+    case 0x35L << 18:
+    case 0x37L << 18:
+    case 0x3AL << 18:
+    case 0x3BL << 18:
+    case 0x3CL << 18:
+    case 0x3DL << 18:
+    case 0x3EL << 18:
+    case 0x3FL << 18:
       continue;
+      break;
+    case 0x06L << 18:
+    case 0x07L << 18:
+    case 0x09L << 18:
+    case 0x0BL << 18:
+    case 0x0DL << 18:
+    case 0x0EL << 18:
+    case 0x12L << 18:
+    case 0x13L << 18:
+    case 0x18L << 18:
+    case 0x1BL << 18:
+    case 0x1CL << 18:
+    case 0x1EL << 18:
+    case 0x21L << 18:
+    case 0x23L << 18:
+    case 0x24L << 18:
+    case 0x27L << 18:
+    case 0x2CL << 18:
+    case 0x2DL << 18:
+    case 0x31L << 18:
+    case 0x32L << 18:
+    case 0x34L << 18:
+    case 0x36L << 18:
+    case 0x38L << 18:
+    case 0x39L << 18:
+      break;
+    default:
+      std::cerr << __FILE__ << __LINE__ << '\n';
+      abort();
     }
-    if ((iteration & 0x00000000000C0000) == (iteration & 0x0000000000C00000)) {
+    // Group E
+    switch (iteration & 0x000000003F000000) {
+    case 0x00 << 24:
+    case 0x01 << 24:
+    case 0x02 << 24:
+    case 0x03 << 24:
+    case 0x04 << 24:
+    case 0x05 << 24:
+    case 0x08 << 24:
+    case 0x0A << 24:
+    case 0x0C << 24:
+    case 0x0F << 24:
+    case 0x10 << 24:
+    case 0x11 << 24:
+    case 0x14 << 24:
+    case 0x15 << 24:
+    case 0x16 << 24:
+    case 0x17 << 24:
+    case 0x19 << 24:
+    case 0x1A << 24:
+    case 0x1D << 24:
+    case 0x1F << 24:
+    case 0x20 << 24:
+    case 0x22 << 24:
+    case 0x25 << 24:
+    case 0x26 << 24:
+    case 0x28 << 24:
+    case 0x29 << 24:
+    case 0x2A << 24:
+    case 0x2B << 24:
+    case 0x2E << 24:
+    case 0x2F << 24:
+    case 0x30 << 24:
+    case 0x33 << 24:
+    case 0x35 << 24:
+    case 0x37 << 24:
+    case 0x3A << 24:
+    case 0x3B << 24:
+    case 0x3C << 24:
+    case 0x3D << 24:
+    case 0x3E << 24:
+    case 0x3F << 24:
       continue;
+      break;
+    case 0x06 << 24:
+    case 0x07 << 24:
+    case 0x09 << 24:
+    case 0x0B << 24:
+    case 0x0D << 24:
+    case 0x0E << 24:
+    case 0x12 << 24:
+    case 0x13 << 24:
+    case 0x18 << 24:
+    case 0x1B << 24:
+    case 0x1C << 24:
+    case 0x1E << 24:
+    case 0x21 << 24:
+    case 0x23 << 24:
+    case 0x24 << 24:
+    case 0x27 << 24:
+    case 0x2C << 24:
+    case 0x2D << 24:
+    case 0x31 << 24:
+    case 0x32 << 24:
+    case 0x34 << 24:
+    case 0x36 << 24:
+    case 0x38 << 24:
+    case 0x39 << 24:
+      break;
+    default:
+      std::cerr << __FILE__ << __LINE__ << '\n';
+      abort();
     }
-    if ((iteration & 0x00000000000C0000) == (iteration & 0x0000000000300000)) {
+    // Group F
+    switch (iteration & 0x0000000FC0000000) {
+    case 0x00L << 30:
+    case 0x01L << 30:
+    case 0x02L << 30:
+    case 0x03L << 30:
+    case 0x04L << 30:
+    case 0x05L << 30:
+    case 0x08L << 30:
+    case 0x0AL << 30:
+    case 0x0CL << 30:
+    case 0x0FL << 30:
+    case 0x10L << 30:
+    case 0x11L << 30:
+    case 0x14L << 30:
+    case 0x15L << 30:
+    case 0x16L << 30:
+    case 0x17L << 30:
+    case 0x19L << 30:
+    case 0x1AL << 30:
+    case 0x1DL << 30:
+    case 0x1FL << 30:
+    case 0x20L << 30:
+    case 0x22L << 30:
+    case 0x25L << 30:
+    case 0x26L << 30:
+    case 0x28L << 30:
+    case 0x29L << 30:
+    case 0x2AL << 30:
+    case 0x2BL << 30:
+    case 0x2EL << 30:
+    case 0x2FL << 30:
+    case 0x30L << 30:
+    case 0x33L << 30:
+    case 0x35L << 30:
+    case 0x37L << 30:
+    case 0x3AL << 30:
+    case 0x3BL << 30:
+    case 0x3CL << 30:
+    case 0x3DL << 30:
+    case 0x3EL << 30:
+    case 0x3FL << 30:
       continue;
+      break;
+    case 0x06L << 30:
+    case 0x07L << 30:
+    case 0x09L << 30:
+    case 0x0BL << 30:
+    case 0x0DL << 30:
+    case 0x0EL << 30:
+    case 0x12L << 30:
+    case 0x13L << 30:
+    case 0x18L << 30:
+    case 0x1BL << 30:
+    case 0x1CL << 30:
+    case 0x1EL << 30:
+    case 0x21L << 30:
+    case 0x23L << 30:
+    case 0x24L << 30:
+    case 0x27L << 30:
+    case 0x2CL << 30:
+    case 0x2DL << 30:
+    case 0x31L << 30:
+    case 0x32L << 30:
+    case 0x34L << 30:
+    case 0x36L << 30:
+    case 0x38L << 30:
+    case 0x39L << 30:
+      break;
+    default:
+      std::cerr << __FILE__ << __LINE__ << '\n';
+      abort();
     }
-    if (++printout % 100000000L == 0) {
-      std::cout << iteration / 10000000L << ' ' << upperlimit / 10000000L << '\n';
+    if (++printout % 1000000L == 0) {
+      std::ios init(NULL);
+      init.copyfmt(std::cout);
+      std::cout << std::hex;
+      std::cout.width(14);
+      std::cout << iteration << ' ';
+      std::cout << upperlimit << '\n';
+      std::cout.copyfmt(init); // restore default formatting
     }
   }
 }
